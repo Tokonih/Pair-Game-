9bc3907d137d10dff13f2ffe6b0eb10db86a4b08
TOKONIH <tokonihstandwell@gmail.com>
2023-05-09 23:58:37 +0100

    i ran a loop through every button and made an eventListerner to respond with a random number when clicked

--- index.js
+++ index.js
@@ -2,28 +2,51 @@
 
 // Declear all characters
 
-const charactersArr = ["Tokonih", "Gerald","Dambola", "Micheal", "Abdul", "Aisha"]
-
-function generateNames(){
+const charactersArr = ["Tokonih", "Gerald", "Dambola", "Micheal", "Abdul", "Aisha"]
+let point = 3
+function generateNames() {
     let result = ""
-    let characterlength = charactersArr.length;    
-    let index =  Math.floor(Math.random()*characterlength);       
-    result  = charactersArr[index];  
-//    return result
+    let characterlength = charactersArr.length;
+    let index = Math.floor(Math.random() * characterlength);
+    result = charactersArr[index];
+    //    return result
 
-   function start(){
-    let word = document.getElementById("word")
-      word.innerHTML = result
-  }
-   start()
+    function start() {
+        let word = document.getElementById("word")
+        word.innerHTML = result
+    }
+    return start()
 }
 
-console.log(generateNames())
+let generate = generateNames()
 
-document.getElementById("btn").onclick = () => {
-    // alert("working")
-    generateNames()
+// document.getElementById("btn").onclick = () => {
+//     // alert("working")
+//     generateNames()
+// }
+
+
+
+
+let select = document.querySelectorAll(".select")
+for (let i = 0; i < select.length; i++) {
+    let names = ""
+    let character = charactersArr.length
+    let index = Math.floor(Math.random() * character)
+    names = charactersArr[index]
+
+
+    select[i].addEventListener("click", () => {
+        select[i].innerHTML = names
+       
+    })
+    
+    
 }
+
+// if (result.value == names.value){
+//     alert("")
+// }
 
 // function start(){
 //   let word = document.getElementById("word")
